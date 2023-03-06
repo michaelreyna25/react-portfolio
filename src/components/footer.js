793a8262ba5e025cf9a682ea69753cc5067bc7fe
Michael Reyna <25.27.reyna@gmail.com>
2023-03-06 11:21:03 -0700

    added footer and navbar

---
+++ src/components/footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function footer() {
+    return (
+        <footer>
+            <div className="git-hub">
+                <img href="https://github.com/michalreyna25"
+                    src="../../public/github-mark/github-mark.png"
+                    alt="github profile picture"
+                />
+            </div>
+            <div className="linked">
+                <img 
+                href="https://www.linkedin.com/in/michael-reyna-35b597245/"
+                src="../../public/icons8-linkedin-circled-48.png"
+                alt="linkedin profile"
+                />
+            </div>
+            <div className="stack-overflow">
+                <img 
+                    href=""
+                    src="../../public/stcak-overflow.png"
+                    alt="stack-overflow link"
+                />
+            </div>
+        </footer>
+    );
+} 
+
+export default footer;
