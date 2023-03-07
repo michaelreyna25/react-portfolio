9a5eee0c6ebc39edc1f98a4e1e53501de602883c
Michael Reyna <25.27.reyna@gmail.com>
2023-03-06 23:37:27 -0700

    finished contact

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -7,24 +7,24 @@ export default function Portfolio() {
             <div className="container justify-content">
                 <ul className="row">
                     <li className="col-4">
-                        <img src="" href="" alt=""/>
+                        <img src="../../public/Show-Loader-During-Image-Loading-vue-load-image.png" href="https://github.com/michaelreyna25/ShadowLands" alt="Shadowlands project"/>
                     </li>
                     <li className="col-4">
-                    <img src="" href="" alt=""/>
+                    <img src="../../public/Show-Loader-During-Image-Loading-vue-load-image.png" href="https://github.com/michaelreyna25/pre-work-studyguide" alt="first project"/>
                     </li>
                     <li className="col-4">
-                    <img src="" href="" alt=""/>
+                    <img src="../../public/Show-Loader-During-Image-Loading-vue-load-image.png" href="https://github.com/megellman/group-project" alt="first group project"/>
                     </li>
                 </ul>
                 <ul className="row">
                     <li className="col-4">
-                    <img src="" href="" alt=""/>
+                    <img src="../../public/Show-Loader-During-Image-Loading-vue-load-image.png" href="https://github.com/michaelreyna25/portfolio" alt="regular portfolio"/>
                     </li>
                     <li className="col-4">
-                    <img src="" href="" alt=""/>
+                    <img src="../../public/Show-Loader-During-Image-Loading-vue-load-image.png" href="https://github.com/michaelreyna25/stop-shop" alt="stop shop"/>
                     </li>
                     <li className="col-4">
-                    <img src="" href="" alt=""/>
+                    <img src="../../public/Show-Loader-During-Image-Loading-vue-load-image.png" href="https://github.com/michaelreyna25/Generate-My-Team" alt="back end project"/>
                     </li>
                 </ul>
             </div>
